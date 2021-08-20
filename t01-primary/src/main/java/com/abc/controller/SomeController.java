8229ba7a55b6fbc59501bd2b9aef8f25a52fa54b
tangyunhang <18815292164@163.com>
2021-08-20 15:48:32 +0800

    springBoot-v1.1-20210813 add t16-tyhmeleaf

--- t01-primary/src/main/java/com/abc/controller/SomeController.java
+++ t01-primary/src/main/java/com/abc/controller/SomeController.java
@@ -1,6 +1,7 @@
 package com.abc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class SomeController {
     @GetMapping("/some")
     public String someHandler(){
         return "hello world spring boot";
+    }
+
+    @GetMapping("/find/{id}")
+    public String findHandler(@PathVariable("id") int id){
+        return "查询指定的学生："+id;
     }
 }
