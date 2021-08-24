93d57c905da87f6c02fc790d9ae4b3de0687a93f
tangyunhang <34445866+tangyunhang@users.noreply.github.com>
2021-08-24 16:57:47 +0800

    Revert "Spring boot v2.0 20210819 (#5)"
    This reverts commit a2fcaccef34a59e4ad9a1d22d0064c15675cc54f.

--- t01-primary/src/main/java/com/abc/controller/SomeController.java
+++ t01-primary/src/main/java/com/abc/controller/SomeController.java
@@ -1,7 +1,6 @@
 package com.abc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,9 +9,5 @@ public class SomeController {
     @GetMapping("/some")
     public String someHandler(){
         return "hello world spring boot";
-    }
-    @GetMapping("/find/{name}")
-    public String findHandler(@PathVariable("name") String name){
-        return "查询指定的学生："+name;
     }
 }
