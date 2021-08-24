2c7de6e7d396114631b0a78164176e453b34768c
tangyunhang <18815292164@163.com>
2021-08-24 20:34:36 +0800

    springBoot-v2-20210824 add t16-tyhmeleaf2

---
+++ t16-thymeleaf/src/main/java/com/abc/vo/Student.java
@@ -0,0 +1,13 @@
+package com.abc.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private int age;
+}
---
+++ t16-thymeleaf2/src/main/java/com/abc/controller/Some2Comtroller.java
@@ -0,0 +1,39 @@
+package com.abc.controller;
+
+import com.abc.bean.Student;
+import com.abc.bean.User;
+import com.abc.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/some2")
+public class Some2Comtroller {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/register")
+    public String someHandler(User student , Model model) throws Exception{
+        //studentService.addStudent(student);
+        model.addAttribute("student",student);
+        return "jsp/welcome";
+    }
+    @PostMapping("/find")
+    @ResponseBody
+    public List<Student> findHandler(int age) {
+        return studentService.findStudentsByAge(age);
+    }
+
+    @GetMapping("/count")
+    @ResponseBody
+    public Integer countHandler() {
+        return studentService.findStudentsCount();
+    }
+}
