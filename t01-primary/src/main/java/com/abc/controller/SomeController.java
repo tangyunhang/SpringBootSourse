a2fcaccef34a59e4ad9a1d22d0064c15675cc54f
tangyunhang <34445866+tangyunhang@users.noreply.github.com>
2021-08-24 16:54:28 +0800

    Spring boot v2.0 20210819 (#5)
    * springBoot-v1.1-20210813 add t1-primary
    
    * springBoot-v1.1-20210813 add t16-tyhmeleaf
    
    * springBoot-v1.1-20210813 add t16-tyhmeleaf
    
    * springBoot-v1.1-20210813 add t16-tyhmeleaf
    
    * Delete t10-ssdm directory

--- t01-primary/src/main/java/com/abc/controller/SomeController.java
+++ t01-primary/src/main/java/com/abc/controller/SomeController.java
@@ -1,6 +1,7 @@
 package com.abc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,9 @@ public class SomeController {
     @GetMapping("/some")
     public String someHandler(){
         return "hello world spring boot";
+    }
+    @GetMapping("/find/{name}")
+    public String findHandler(@PathVariable("name") String name){
+        return "查询指定的学生："+name;
     }
 }
