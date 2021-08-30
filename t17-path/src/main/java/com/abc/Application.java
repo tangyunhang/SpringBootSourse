b5a9c5c1595f41d90417d24378e2a504a3b82376
tangyunhang <18815292164@163.com>
2021-08-30 15:12:41 +0800

    springBoot-v2-20210824 add t17-path

--- t17-path/src/main/java/com/abc/Application.java
+++ t17-path/src/main/java/com/abc/Application.java
@@ -1,8 +1,12 @@
 package com.abc;
 
+import com.abc.contextInitializer.TestApplicationContextInitializer;
+import com.abc.contextInitializer.TestApplicationContextInitializer2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.*;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationListener;
 
 //启动类
 @EnableCaching //开启缓存
@@ -10,7 +14,38 @@ import org.springframework.cache.annotation.EnableCaching;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        //SpringApplication.run(Application.class, args);
+        SpringApplication springApplication=new SpringApplication(Application.class);
+        springApplication.addListeners((ApplicationListener<ApplicationStartingEvent>) event->{
+            System.out.println("Starting");
+        });
+        springApplication.addListeners((ApplicationListener<ApplicationStartedEvent>) event->{
+            System.out.println("Started");
+        });
+        springApplication.addListeners((ApplicationListener<ApplicationFailedEvent>) event->{
+            System.out.println("Failed");
+        });
+        springApplication.addListeners((ApplicationListener<ApplicationPreparedEvent>) event->{
+            System.out.println("Prepared");
+        });
+
+        springApplication.addListeners((ApplicationListener<SpringApplicationEvent>) event->{
+            System.out.println("SpringApplication");
+        });
+
+        springApplication.addListeners((ApplicationListener<ApplicationEnvironmentPreparedEvent>) event->{
+            System.out.println("EnvironmentPrepare");
+        });
+
+        springApplication.addListeners((ApplicationListener<ApplicationReadyEvent>) event->{
+            System.out.println("Ready");
+        });
+        springApplication.addInitializers(new TestApplicationContextInitializer());
+        springApplication.addInitializers(new TestApplicationContextInitializer2());
+
+
+
+        springApplication.run(args);
     }
 
 }
